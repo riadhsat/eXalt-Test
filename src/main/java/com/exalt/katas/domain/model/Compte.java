55b9c36f5b637b8b2b19f42350a954a1ba2172f3
Riadh TOUNAKTI <riadh.tounakti@gmail.com>
2023-02-23 14:33:11 +0100

    implementation deposit Amount and test ok

--- src/main/java/com/exalt/katas/domain/model/Compte.java
+++ src/main/java/com/exalt/katas/domain/model/Compte.java
@@ -3,9 +3,11 @@ package com.exalt.katas.domain.model;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Builder
 @Getter
+@Setter
 public class Compte {
 
   private String id;
--- src/main/java/com/exalt/katas/domain/service/CompteService.java
+++ src/main/java/com/exalt/katas/domain/service/CompteService.java
@@ -6,8 +6,10 @@
 package com.exalt.katas.domain.service;
 
 import com.exalt.katas.domain.api.CompteServicePort;
+import com.exalt.katas.domain.model.Compte;
 import com.exalt.katas.domain.spi.PersistancePort;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,6 +20,8 @@ public class CompteService implements CompteServicePort {
 
   @Override
   public void depositMoney(double amount) {
-
+    Compte compte = persistancePort.findCompte();
+    compte.setSolde(compte.getSolde() + amount);
+    persistancePort.updateCompte(compte);
   }
 }
