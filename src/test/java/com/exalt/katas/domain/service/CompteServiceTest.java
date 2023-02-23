a15a972f5086c7433f64ba0e565ff2d274665a8b
Riadh TOUNAKTI <riadh.tounakti@gmail.com>
2023-02-23 15:54:51 +0100

    test ko pour ajouter la transaction deposit

--- src/test/java/com/exalt/katas/domain/service/CompteServiceTest.java
+++ src/test/java/com/exalt/katas/domain/service/CompteServiceTest.java
@@ -2,17 +2,19 @@ package com.exalt.katas.domain.service;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 import com.exalt.katas.domain.model.Compte;
+import com.exalt.katas.domain.model.Status;
+import com.exalt.katas.domain.model.Transaction;
+import com.exalt.katas.domain.model.TypeTransaction;
 import com.exalt.katas.domain.spi.PersistancePort;
 import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-
-import static org.mockito.Mockito.when;
 
 class CompteServiceTest {
 
@@ -39,5 +41,27 @@ class CompteServiceTest {
     compteService.depositMoney(120);
 
     assertThat(compte.getSolde()).isEqualTo(120);
+  }
+
+  @Test
+  void depositMoney_with_120_solde_is_0_update_solde_to_120_and_ADD_Transaction_with_status_valid() {
+
+    Compte compte = Compte.builder()
+        .id("id").solde(0).transactions(new ArrayList<>())
+        .build();
+    when(persistancePort.findCompte()).thenReturn(compte);
+    when(persistancePort.updateCompte(compte)).thenReturn(compte);
+
+    compteService.depositMoney(120);
+
+    assertThat(compte.getSolde()).isEqualTo(120);
+    assertThat(compte.getTransactions()).isNotEmpty();
+    assertThat(compte.getTransactions()).usingRecursiveComparison().ignoringFields("creationDate")
+        .isEqualTo(List.of(Transaction.builder()
+            .typeTransaction(TypeTransaction.DEPOSIT)
+            .montant(120)
+            .status(Status.VALID)
+            .description("transaction validé")
+            .build()));
   }
 }
