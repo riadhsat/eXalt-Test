d51d8fe07317e84af677b7a426837f2f596f9e30
Riadh TOUNAKTI <riadh.tounakti@gmail.com>
2023-02-24 10:32:59 +0100

    test ko controller  (retrait de solde)

--- src/main/java/com/exalt/katas/application/controller/CompteController.java
+++ src/main/java/com/exalt/katas/application/controller/CompteController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,11 @@ public class CompteController {
   public ResponseEntity<ResultResponse> consultSolde(){
     double solde = compteServicePort.consultBalance();
     return ResponseEntity.ok(ResultResponse.builder().message("Votre solde est :"+solde).build());
+  }
+
+  @PostMapping("/withdrawal")
+  public ResponseEntity<ResultResponse> withdrawalMoney(){
+    return null;
   }
 
 }
--- src/test/java/com/exalt/katas/application/controller/CompteControllerTest.java
+++ src/test/java/com/exalt/katas/application/controller/CompteControllerTest.java
@@ -1,5 +1,15 @@
 package com.exalt.katas.application.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
 import com.exalt.katas.domain.api.CompteServicePort;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,13 +18,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 class CompteControllerTest {
 
@@ -38,12 +41,27 @@ class CompteControllerTest {
 
     when(compteServicePort.consultBalance()).thenReturn(15.0);
 
-    MvcResult result =mockMvc.perform(get("/compte"))
+    MvcResult result = mockMvc.perform(get("/compte"))
         .andExpect(status().isOk())
         .andReturn();
 
     assertNotNull(result.getResponse().getContentAsString());
     assertThat(result.getResponse().getContentAsString()).isEqualTo("{\"message\":\"Votre solde est :15.0\"}");
+
+  }
+
+  @Test
+  void testWithdrawalMoney() throws Exception {
+
+    MvcResult result = mockMvc.perform(post("/compte/withdrawal")
+        .param("amount", "1500"))
+        .andExpect(status().isOk())
+        .andReturn();
+
+    assertNotNull(result.getResponse().getContentAsString());
+    assertThat(result.getResponse().getContentAsString())
+        .isEqualTo("{\"message\":\"Votre retrait de montant 1500 a ete effectué avec succès\"}");
+    verify(compteServicePort, only()).withdrawalMoney(1500);
 
   }
 
